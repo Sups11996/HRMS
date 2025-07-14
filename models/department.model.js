f46550fc1efbf195f78dde618d55e34667a9d5a6
Sudip Khadka <sups11996@gmail.com>
2025-07-14 10:28:52 +0545

    learning git

---
+++ models/department.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const DepartmentSchema = new mongoose.Schema({
+    _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    auto: true, 
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+   managerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employee',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Department = mongoose.model('Department', DepartmentSchema);
